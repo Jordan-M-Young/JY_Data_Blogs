ad0cf745a1f073e66a5f4fa86537432fec4f4dda
Jordan M. Young <54070169+Jordan-M-Young@users.noreply.github.com>
2020-11-08 10:24:43 -0500

    Update fig_5.py

--- Safe!_MediumBlog1/fig_5.py
+++ Safe!_MediumBlog1/fig_5.py
@@ -9,13 +9,6 @@ Created on Fri Oct 30 16:53:16 2020
 import pandas as pd
 import matplotlib.pyplot as plt
 from scipy.stats import linregress as linreg
-import numpy as np
-
-
-#Opens player hitting statistics
-lf_hitting = pd.read_csv('C:/Users/jmyou/Desktop/bb_data_project/Left_Hitter_basic_hitting_Stats.csv')
-rf_hitting = pd.read_csv('C:/Users/jmyou/Desktop/bb_data_project/Right_Hitter_basic_hitting_Stats.csv')
-s_hitting = pd.read_csv('C:/Users/jmyou/Desktop/bb_data_project/Switch_Hitter_basic_hitting_Stats.csv')
 
 
 #Opens player running statistics
@@ -29,22 +22,24 @@ lf_running = lf_running.dropna(axis=0)
 rf_running = rf_running.dropna(axis=0)
 s_running = s_running.dropna(axis=0)
 
+#Opens player attributes 
 l_attributes = pd.read_csv('C:/Users/jmyou/Desktop/bb_data_project/Left_Hitter_player_attributes.csv')
 r_attributes = pd.read_csv('C:/Users/jmyou/Desktop/bb_data_project/Right_Hitter_player_attributes.csv')
 s_attributes = pd.read_csv('C:/Users/jmyou/Desktop/bb_data_project/Switch_Hitter_player_attributes.csv')
 
+#Gets column names needed for dataframe joins
 lc = lf_running.columns
 rc = rf_running.columns
 sc = s_running.columns
 
 
-#Linear regressions for each group of batters
+#Linear regressions for each group of batters; these are the same regressions from figure 4.
 l_reg = linreg(lf_running.loc[:,'sprint_speed'],lf_running.loc[:,'hp_to_1b'])
 r_reg = linreg(rf_running.loc[:,'sprint_speed'],rf_running.loc[:,'hp_to_1b'])
 s_reg = linreg(s_running.loc[:,'sprint_speed'],s_running.loc[:,'hp_to_1b'])
 
 
-
+#Joins player attribute and player running dataframes for easier analysis
 new_l = pd.merge(l_attributes,lf_running,
                  left_on=['nameLast','nameFirst']
                  ,right_on=['last_name', lc[1]])
@@ -57,27 +52,23 @@ new_s = pd.merge(s_attributes,s_running,
                  left_on=['nameLast','nameFirst']
                  ,right_on=['last_name', sc[1]])
 
+#Gets time to first base for each player according to the best fit lines generated in fig 4.
 l_iT = new_l.loc[:,'sprint_speed']*l_reg[0] + l_reg[1]
 r_iT = new_r.loc[:,'sprint_speed']*r_reg[0] + r_reg[1]
 s_iT = new_s.loc[:,'sprint_speed']*s_reg[0] + s_reg[1]
 
 
+#Time differential
 l_dt = l_iT - new_l.loc[:,'hp_to_1b']
 r_dt = r_iT - new_r.loc[:,'hp_to_1b']
 s_dt = s_iT - new_s.loc[:,'hp_to_1b']
 
+#Player BMI calculations
 l_wt = (new_l.loc[:,'weight'] / 2.20462) / ((new_l.loc[:,'height'] / 39.3701)**2)
 r_wt = (new_r.loc[:,'weight'] / 2.20462) / ((new_r.loc[:,'height'] / 39.3701)**2)
 s_wt = (new_s.loc[:,'weight'] / 2.20462) / ((new_s.loc[:,'height'] / 39.3701)**2)
 
-l_reg2 = linreg(l_dt,l_wt)
-r_reg2 = linreg(r_dt,r_wt)
-s_reg2 = linreg(s_dt,s_wt)
 
-x = np.array([i/100 for i in range(-10,30)])
-yr = r_reg2[0]*x + r_reg2[1]
-yl = l_reg2[0]*x + l_reg2[1]
-ys = s_reg2[0]*x + s_reg2[1]
 
 #Viridis Colors
 vir_v = (71/255, 33/255, 115/255) 
@@ -86,7 +77,7 @@ vir_g = (40/255, 175/255, 126/255)
 vir_y =  (188/255, 223/255, 37/255)
 
 
-
+#Figure 5.
 plt.scatter(l_dt,l_wt,color=vir_v,zorder=3,edgecolors='black',s=60)
 plt.scatter(r_dt,r_wt,color=vir_g,zorder=3,edgecolors='black',s=60)
 plt.scatter(s_dt,s_wt,color=vir_y,zorder=3,edgecolors='black',s=60)
